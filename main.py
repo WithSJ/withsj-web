a921443aca2eca82ad117a0086f25e287fa334b7
WithSJ <withsj.in@gmail.com>
2020-05-13 12:46:59 +0530

    structure complete but navbar in devlopment

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/blogs")
+def blogs():
+    return render_template('blogs.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
