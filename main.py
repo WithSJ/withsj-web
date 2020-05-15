f96a7d49d3c86eb26411ca409c67b0895966182e
WithSJ <withsj.in@gmail.com>
2020-05-15 14:21:17 +0530

    blogpage added

--- main.py
+++ main.py
@@ -19,5 +19,10 @@ def portfolio():
 def about():
     return render_template('about.html')
 
+@app.route("/blogs/<string:name>")
+def blogpage(name):
+    return render_template('blogpage.html',name=name)
+    
+
 if __name__ == "__main__":
     app.run(debug=True)
