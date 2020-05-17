3edf1dc73d86cbb0d886745fe37c0f78b9348795
WithSJ <withsj.in@gmail.com>
2020-05-17 15:54:11 +0530

    created get_posts it give data from database

---
+++ web_database/get_posts.py
@@ -0,0 +1,18 @@
+from web_database import connect_database
+
+class Get_Blogs():
+    def data(self):
+        """Get all data from blogs table"""
+        conn = connect_database()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Blogs")
+        return cur.fetchall()
+
+class Get_Portfolio():
+    def data(self):
+        """Get all data from portfolio table"""
+        conn = connect_database()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Portfolio")
+        return cur.fetchall()
+
