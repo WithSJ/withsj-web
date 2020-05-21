69487254c894adb1c2c8dc7dacaffddbb915a057
WithSJ <withsj.in@gmail.com>
2020-05-21 16:41:16 +0530

    update install

--- install.py
+++ install.py
@@ -1,8 +1,8 @@
 """Install all pkgs """
 from web_database.configdatabse import Create_WithSJ_Database
 import os
-# os.system("pip install bcrypt")
-
+os.system("pip install bcrypt")
+os.system("pip install flask_wtf")
 Create_WithSJ_Database()
 
 
