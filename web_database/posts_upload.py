20a40d4da4d1f9a9c289ad5f0cd5240ae96fd32e
WithSJ <withsj.in@gmail.com>
2020-05-23 08:45:53 +0530

    dynamic link of blog update

--- web_database/posts_upload.py
+++ web_database/posts_upload.py
@@ -7,18 +7,26 @@ class BlogPost():
         self.post = post    
 
     def upload(self):
+        rand_link = hex(randint(0,32))[2:]
         conn = connect_database()
         conn.execute("""
         INSERT INTO Blogs(Title,Date,Post,BlogID)
         VALUES(:title,:date,:post,:blogid)
-        """,{"title":self.title,"date":self.date,"post":self.post,"blogid":self.get_blogid()})
+        """,{"title":self.title,"date":self.date,"post":self.post,"blogid":self.get_blogid(h_link=rand_link)})
         conn.commit()
         conn.close()
 
     def get_blogid(self,h_link = hex(randint(0,32))[2:] ):
         blogid = self.title.replace(" ","%") +"%"+h_link
-        return blogid
-
+        conn = connect_database()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Blogs WHERE BlogID = :blogid", {"blogid":blogid})
+        data = cur.fetchone()
+        if data == None:
+            return blogid
+        else:
+            h_link=hex(randint(0,32*2))[2:]
+            self.get_blogid(h_link)
 
 class Portfolio(BlogPost):
     def upload(self):
